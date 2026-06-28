73d1a9d56b2d7c0f34c54132e7df32dcf63a0231
Hemal Buha <hemal@edmingle.com>
2026-06-28 12:31:12 +0530

    ZyncSpace v2 website

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -125,6 +125,7 @@ export default function Navbar() {
 
   useEffect(() => {
     const onScroll = () => setScrolled(window.scrollY > 40);
+    onScroll();
     window.addEventListener('scroll', onScroll, { passive: true });
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
